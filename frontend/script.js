0b022a9b91309b92220fe5cfc640a4622393b48c
Batuhan Keskin <125993959+Makvvaize@users.noreply.github.com>
2024-12-07 16:28:22 +0300

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,6 +1,6 @@
-const APILINK = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=4ceceb34c39aa1bafb70be15dfdf6b7b&page=1';
+const APILINK = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=[API_KEY]&page=1';
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
-const SEARCHAPI = 'https://api.themoviedb.org/3/search/movie?&api_key=4ceceb34c39aa1bafb70be15dfdf6b7b&query=';
+const SEARCHAPI = 'https://api.themoviedb.org/3/search/movie?&api_key=[API_KEY]&query=';
 
 const main = document.getElementById("section");
 const form = document.getElementById("form");
